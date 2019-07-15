baee0237431d2799e4a938c9ce88d5b0118ce349
Matt <mattpaul001@icloud.com>
2019-07-15 21:56:26 +0100

    Volume

--- src/components/on-off-button/index.jsx
+++ src/components/on-off-button/index.jsx
@@ -2,27 +2,16 @@ import React from 'react';
 
 const OnOffButton = ({toggleOnOff, on}) => (
   <button style={styles.button} onClick={toggleOnOff}>
-    {on
-      ? <svg width="33" height="42" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M1 1v40h31V1z"
-            fill="#900921"
-            stroke="#250A0F"
-            fill-rule="evenodd"
-            stroke-linejoin="round"
-          />
-        </svg>
-      : <svg width="33" height="42" xmlns="http://www.w3.org/2000/svg">
-          <path
-            d="M1 1v40h31V1z"
-            fill="#900921"
-            stroke="#250A0F"
-            stroke-width="2"
-            fill-rule="evenodd"
-            stroke-linejoin="round"
-          />
-        </svg>}
-
+    <svg width="33" height="42" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M1 1v40h31V1z"
+        fill="#900921"
+        stroke="#250A0F"
+        strokeWidth={on ? '1' : '2'}
+        fillRule="evenodd"
+        strokeLinejoin="round"
+      />
+    </svg>
   </button>
 );
 
--- src/components/volume-knob/index.jsx
+++ src/components/volume-knob/index.jsx
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+const useKnobPosition = () => {
+  const [position, setPosition] = useState ({x: 0, y: 0});
 
-const VolumeKnob = () => (
-  <button>
-    <svg width="79" height="79" xmlns="http://www.w3.org/2000/svg">
-      <g transform="translate(-2 -2)" fill="none" fillRule="evenodd">
-        <circle stroke="#795F4B" fill="#947961" cx="41.5" cy="41.5" r="38.5" />
-        <path
-          d="M36 52L23 73"
-          stroke="#1D1313"
-          stroke-width="4"
-          stroke-linecap="square"
-        />
-      </g>
-    </svg>
+  useEffect (() => {
+    const setFromEvent = event =>
+      setPosition ({x: event.clientX, y: event.clientY});
+    window.addEventListener ('mousedown', setFromEvent);
 
-  </button>
-);
+    return () => {
+      window.removeEventListener ('mousedown', setFromEvent);
+    };
+  }, []);
+
+  return position;
+};
+
+const VolumeKnob = () => {
+  const position = useKnobPosition ();
+
+  return (
+    <button>
+      <div>{position.x}: {position.y}</div>
+      <svg width="79" height="79" xmlns="http://www.w3.org/2000/svg">
+        <g transform="translate(-2 -2)" fill="none" fillRule="evenodd">
+          <circle
+            stroke="#795F4B"
+            fill="#947961"
+            cx="41.5"
+            cy="41.5"
+            r="38.5"
+          />
+          <path
+            d="M36 52L23 73"
+            stroke="#1D1313"
+            strokeWidth="4"
+            strokeLinecap="square"
+          />
+        </g>
+      </svg>
+    </button>
+  );
+};
 
 export default VolumeKnob;
